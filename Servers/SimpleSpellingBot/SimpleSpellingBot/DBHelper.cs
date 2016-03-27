a9204ec1246b1206215db0cfe88b113033b315b0
Robert Woodley <blwoodley@yahoo.com>
2016-03-27 14:24:06 -0500

    misc check-ins

---
+++ Servers/SimpleSpellingBot/SimpleSpellingBot/DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleSpellingBot
+{
+    public class DBHelper
+    {
+        public static void UpdateAssignment(
+                    int assignmentid,
+                    DateTime effectiveDate,
+                    DateTime expirationDate,
+                    String name,
+                    List<Word> words
+                    ) {
+            Console.Write(effectiveDate);
+        }
+    }
+}
