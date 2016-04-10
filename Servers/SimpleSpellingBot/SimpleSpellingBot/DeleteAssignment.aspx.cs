54b1584269ac98b7eb89dbb0db8ea2daabe33482
Robert Woodley <blwoodley@yahoo.com>
2016-04-10 11:18:41 -0500

    adding delete assignment functionality

---
+++ Servers/SimpleSpellingBot/SimpleSpellingBot/DeleteAssignment.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleSpellingBot
+{
+    public partial class DeleteAssignment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var db = new DataClasses1DataContext();
+
+            if (Request.HttpMethod.Equals("GET"))
+            {
+                int testid = int.Parse(Request.QueryString["id"]);
+                Assignment ass = db.Assignments.Where(x=>x.AssignmentID == testid).Single();
+                ass.IsDeleted = true;
+                db.SubmitChanges();
+
+                Response.Clear();
+                Response.AppendHeader("Access-Control-Allow-Origin", "*");
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.Write("[]");
+                Response.End();
+            }
+        }
+    }
+}
